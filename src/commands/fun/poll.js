65d518c1705088695d67c3c25a7df8a9ecb1a410
DapperNurd <jbenbonus@gmail.com>
2023-07-28 03:38:26 -0700

    Updated poll command changed from interaction reply to send message added a length of time parameter that goes from 1 minute to 2 hours added dynamic color picking for the progress bar which shows the highest

--- src/commands/fun/poll.js
+++ src/commands/fun/poll.js
@@ -31,17 +31,28 @@ module.exports = {
             option.setName('fourth')
                   .setDescription('The fourth option people can pick. (Optional)')
                   .setMaxLength(200)
+        )
+        .addStringOption(option =>
+            option.setName('fifth')
+                  .setDescription('The fifth option people can pick. (Optional)')
+                  .setMaxLength(200)
+        )
+        .addIntegerOption(option =>
+            option.setName('length')
+                  .setDescription('The length of time, in minutes, for the poll to last. Default is 30 minutes.')
+                  .setMinValue(1)
+                  .setMaxValue(120)
         ),
     async execute(interaction, client) {
 
         var interacted = [];
-        var totalVotes = 0;
-        var aVotes = 0;
-        var bVotes = 0;
-        var cVotes = 0;
-        var dVotes = 0;
+        var totalVotes = 0, aVotes = 0, bVotes = 0, cVotes = 0, dVotes = 0, eVotes = 0;
 
-        var timeLeft = 15; // Cannot go above 15 because of discord limits
+        const pollLength = interaction.options.getInteger("length") ?? 30;
+
+        const pollTimestamp = Date.now(); // Used to track when the poll was created
+
+        var timeLeft = pollLength; // Decrementing variable used to count time left for poll
         
         // ⬛⬜🟥🟧🟨🟩🟦🟪🟫❎⏹🔲🔳
 
@@ -49,110 +60,99 @@ module.exports = {
         const author = await client.users.fetch("189510396569190401"); // Gets my (nurd) user from my id
 
         // Building the buttons
-        const optionAButton = new ButtonBuilder()
-			.setCustomId('a')
-			.setLabel('A')
-			.setStyle(ButtonStyle.Secondary);
+        const optionAButton = new ButtonBuilder().setCustomId('a').setLabel('A').setStyle(ButtonStyle.Secondary);
+        const optionBButton = new ButtonBuilder().setCustomId('b').setLabel('B').setStyle(ButtonStyle.Secondary);
+        const optionCButton = new ButtonBuilder().setCustomId('c').setLabel('C').setStyle(ButtonStyle.Secondary);
+        const optionDButton = new ButtonBuilder().setCustomId('d').setLabel('D').setStyle(ButtonStyle.Secondary);
+        const optionEButton = new ButtonBuilder().setCustomId('e').setLabel('E').setStyle(ButtonStyle.Secondary);
 
-        const optionBButton = new ButtonBuilder()
-			.setCustomId('b')
-			.setLabel('B')
-			.setStyle(ButtonStyle.Secondary);
+        const endButton = new ButtonBuilder().setCustomId('z').setLabel('End Poll').setStyle(ButtonStyle.Danger);
 
-        const optionCButton = new ButtonBuilder()
-			.setCustomId('c')
-			.setLabel('C')
-			.setStyle(ButtonStyle.Secondary);
+        const voteButtonRow = new ActionRowBuilder().addComponents(optionAButton, optionBButton);
+        const actionButtonRow = new ActionRowBuilder().addComponents(endButton);
 
-        const optionDButton = new ButtonBuilder()
-			.setCustomId('d')
-			.setLabel('D')
-			.setStyle(ButtonStyle.Secondary);
-
-        const endButton = new ButtonBuilder()
-            .setCustomId('x')
-            .setLabel('End Poll')
-            .setStyle(ButtonStyle.Danger);
-
-        const row = new ActionRowBuilder()
-			.addComponents(optionAButton, optionBButton);
-
-        if(interaction.options.getString("third")) row.addComponents(optionCButton);
-        if(interaction.options.getString("fourth")) row.addComponents(optionDButton);
-
-        row.addComponents(endButton);
+        if(interaction.options.getString("third")) voteButtonRow.addComponents(optionCButton);
+        if(interaction.options.getString("fourth")) voteButtonRow.addComponents(optionDButton);
+        if(interaction.options.getString("fifth")) voteButtonRow.addComponents(optionEButton);
 
         // Sends the embed message
-        const pollMsg = await interaction.reply({
-            content: 'Building poll...'
+        const confirmation = await interaction.reply({ // Sends a confirmation reply just to confirm out the interaction
+            content: 'Building poll...',
+            ephemeral: true
         });
         
-        await updateEmbed(false);
-
-        await pollMsg.edit({
+        const pollMsg = await interaction.channel.send({ // Sends an empty message to be used for the embed
             content: '',
-            components: [row]
+            components: [voteButtonRow, actionButtonRow]
         })
 
-        const collector = pollMsg.createMessageComponentCollector({ componentType: ComponentType.Button, time: 880_000 }); // Collector for the buttons, just under 15 minutes
+        await updateEmbed(false, true); // Initial embed updating (part of sending the embed)
 
-        collector.on('collect', async i => {
-            if(interacted.includes(i.user.id) && i.customId != 'x') {
-                return await i.reply({
-                    content:`You have already voted!`,
-                    ephemeral: true
-                });
-            }
-            if(i.customId != 'x') {
-                if(i.customId == 'a')      aVotes++;
-                else if(i.customId == 'b') bVotes++;
-                else if(i.customId == 'c') cVotes++;
-                else if(i.customId == 'd') dVotes++;
-                else                       return i.reply({ content: "I don't know what to do with that button.....", ephemeral: true });
+        // Starts the timer for the message time remaining
+        var timer = setInterval(() => {
+            timeLeft -= 1;
+            updateEmbed();
+        }, 60 * 1000);
 
-                totalVotes++;
-                interacted.push(i.user.id);
-                updateEmbed(false);
-                await i.reply({
-                    content: `Successfully voted for Option ${i.customId.toUpperCase()}`,
-                    ephemeral: true
-                });
-            }
-            else {
+        // Collection handling
+        const collector = pollMsg.createMessageComponentCollector({ componentType: ComponentType.Button, time: pollLength * 60_000 }); // Creating the collector for the buttons
+
+        collector.on('collect', async i => { // Collector on collect function
+            if(interacted.includes(i.user.id) && i.customId != 'z' && i.customId != 'x') return await i.reply({ content:`You have already voted!`, ephemeral: true }); // Checks if the user is incldued in the already interacted users, and that it is not the close poll button
+            if(i.customId == 'z') { // Ending the poll
                 // If the person who pressed the end button is not the user who ran the poll command
                 if(interaction.user.id != i.user.id) return await i.reply({ content:`Only the creator can end the poll!`, ephemeral: true });
                 
                 // Else, end the poll
-                clearInterval(timer); // Stops the interval for counting down timeLeft
                 collector.stop();
-                updateEmbed(true);
+                updateEmbed(true, false);
                 await i.reply({
                     content: `Successfully closed poll.`,
                     ephemeral: true
                 });
             }
+            else {
+                if(i.customId == 'a')      aVotes++; //
+                else if(i.customId == 'b') bVotes++; //
+                else if(i.customId == 'c') cVotes++; // Increments the respective vote
+                else if(i.customId == 'd') dVotes++; //
+                else if(i.customId == 'e') eVotes++; //
+                else                       return i.reply({ content: "I don't know what to do with that button.....", ephemeral: true }); // Should not ever run, but if a button is pressed that is not one of the above ones
+
+                totalVotes++; // Increments the total vote counter
+                interacted.push(i.user.id); // Adds the user (the one who pressed the button) to the array of interacted users
+                updateEmbed(); // Updates the embed
+                await i.reply({ // Tells the user they have successfully voted for some option
+                    content: `Successfully voted for Option ${i.customId.toUpperCase()}`,
+                    ephemeral: true
+                });
+            }
         });
 
-        function buildProgressBar(votes) {
+        collector.on('end', () => { // Collector on end function
+            updateEmbed(true, false);
+        });
+
+        // Functions
+        function buildProgressBar(votes, color) { // Builds the progress var based on the votes, this is really just so I'm not repeating the same code 4 times
             var progress = "";
             var perc = (votes / totalVotes) * 10;
-            if(totalVotes <= 0) perc = 0;
-            for(var i = 0; i < Math.ceil(perc); i++) {
-                progress += "🟩 ";
-            }
-            for(var i = 0; i < Math.floor(10 - perc); i++) {
-                progress += "⬛ ";
-            }
-            progress += ` ${votes}  (${Math.round(100*perc)/10}%)`
 
-            return progress;
+            if(totalVotes <= 0) perc = 0; // This is just to avoid division by zero errors and NaN occurences
+
+            for(var i = 0; i < Math.ceil(perc); i++)       progress += `${color} `;
+            for(var i = 0; i < Math.floor(10 - perc); i++) progress += "⬛ ";
+
+            return progress += ` ${votes}  (${Math.round(100*perc)/10}%)`;
         }
 
-        async function updateEmbed(end) {
-            const aProgressBar = buildProgressBar(aVotes);
-            const bProgressBar = buildProgressBar(bVotes);
-            const cProgressBar = buildProgressBar(cVotes);
-            const dProgressBar = buildProgressBar(dVotes);
+        async function updateEmbed(end = false, start = false) { // Updates the embed with the proper information
+            const highest = Math.max(aVotes, bVotes, cVotes, dVotes, eVotes); // Calculation for the highest voted value
+            const aProgressBar = buildProgressBar(aVotes, (aVotes == highest) ? '🟩' : '🟦'); //
+            const bProgressBar = buildProgressBar(bVotes, (bVotes == highest) ? '🟩' : '🟦'); //
+            const cProgressBar = buildProgressBar(cVotes, (cVotes == highest) ? '🟩' : '🟦'); // Builds progress bar by votes, and determines which emoji to use for the bar based on whether or not it is the highest value
+            const dProgressBar = buildProgressBar(dVotes, (dVotes == highest) ? '🟩' : '🟦'); //
+            const eProgressBar = buildProgressBar(eVotes, (eVotes == highest) ? '🟩' : '🟦'); //
 
             const updatedEmbed = new EmbedBuilder()
                 .setColor(interaction.user.accentColor ?? '#FFFFFF')
@@ -161,52 +161,41 @@ module.exports = {
                     name: `${client.user.username} Polls`
                 })
                 .setTitle(interaction.options.getString("question"))
-                .setDescription(`Poll created by ${userMention(interaction.user.id)}`)
                 //.setThumbnail(interaction.user.displayAvatarURL()) // Removed because it made it not wide enough on mobile for the progress bars
                 .addFields ([
                     { name: `A) ${interaction.options.getString("first")}`, value: aProgressBar },
                     { name: `B) ${interaction.options.getString("second")}`, value: bProgressBar },
                 ])
-                .setTimestamp()
                 .setFooter({
                     iconURL: author.displayAvatarURL(),
                     text: `PiebotV3 by ${author.username}`
-                });
+                })
+                .setTimestamp(pollTimestamp);
 
             if(interaction.options.getString("third")) updatedEmbed.addFields([{ name: `C) ${interaction.options.getString("third")}`, value: cProgressBar }]);
             if(interaction.options.getString("fourth")) updatedEmbed.addFields([{ name: `D) ${interaction.options.getString("fourth")}`, value: dProgressBar }]);
+            if(interaction.options.getString("fifth")) updatedEmbed.addFields([{ name: `E) ${interaction.options.getString("fifth")}`, value: eProgressBar }]);
 
             updatedEmbed.addFields([
                 { name: '\n', value: '\n' },
-                { name: `Total Votes: ${totalVotes}`, value: '\n'},
-                { name: (!end) ? `${timeLeft} minutes remaining` : 'Poll ended.', value: '\n' },
+                { name: `Total Votes: ${totalVotes}`, value: `Poll created by ${userMention(interaction.user.id)}\n`},
+                { name: (!end) ? ( (timeLeft > 1) ? `${timeLeft} minutes remaining` : '< 1 minute remaining' ) : 'Poll ended.', value: '\n' }, // Basically a nested ?: operator... this is super messy so I will break it down... if it is not end, run { if time is less than minute, return timeLeft, else, return <1 minute } else return Poll ended
             ])
 
-            if(end) {
-                optionAButton.setDisabled(true);
-                optionBButton.setDisabled(true);
-                if(interaction.options.getString("third")) optionCButton.setDisabled(true);
-                if(interaction.options.getString("fourth")) optionDButton.setDisabled(true);
-                endButton.setDisabled(true);
+            if(start) { // Only runs on the first posting of the embed
+                confirmation.delete();
+            }
+
+            if(end) { // Only runs on the last editing of the embed
+                clearInterval(timer); // Stops the interval for counting down timeLeft
                 await pollMsg.edit({
-                    components: [row]
-                })
+                    components: []
+                }).catch(err => console.log('Error updating poll embed! (End check)'));
             }
 
             await pollMsg.edit({
                 embeds: [updatedEmbed]
-            })
+            }).catch(err => console.log('Error updating poll embed!'));
         }
-
-        collector.on('end', () => {
-            clearInterval(timer);
-            updateEmbed(true);
-        });
-
-        var timer = setInterval(() => {
-            timeLeft -= 1;
-            updateEmbed(false);
-        }, 60 * 1000);
-
     }
 }
