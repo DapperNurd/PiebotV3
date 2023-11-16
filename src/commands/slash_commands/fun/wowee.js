e3984c332379098f35734c2b6ba00ff39f0b63ac
Ben Bonus <jbenbonus@gmail.com>
2023-11-16 01:22:51 -0800

    added wowee command sorta

---
+++ src/commands/slash_commands/fun/wowee.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('wowee')
+        .setDescription('wow wow'),
+    async execute(interaction, client) {
+
+        // Sends the embed message
+        await interaction.reply({
+            content: '<:nurdThonk:983576552488984586>',
+            ephemeral: true
+        });
+    }
+}
