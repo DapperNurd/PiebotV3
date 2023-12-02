3492ff9af5bf98272054694d978c7c9374f4ac87
Ben Bonus <jbenbonus@gmail.com>
2023-12-01 16:46:08 -0800

    12 to 24 hour limit on polls

--- src/commands/slash_commands/fun/poll.js
+++ src/commands/slash_commands/fun/poll.js
@@ -42,7 +42,7 @@ module.exports = {
             option.setName('length')
                   .setDescription('The length of time, in minutes, for the poll to last. Default is 30 minutes.')
                   .setMinValue(1)
-                  .setMaxValue(60*12)
+                  .setMaxValue(60*24)
         ),
     async execute(interaction, client) {
 
