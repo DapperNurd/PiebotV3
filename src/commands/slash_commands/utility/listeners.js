535caf5eaafdfd19a0f7e1464110ae1b68b5c76e
Ben Bonus <jbenbonus@gmail.com>
2025-03-19 14:53:19 -0700

    Added defer reply to thee bot

--- src/commands/slash_commands/utility/listeners.js
+++ src/commands/slash_commands/utility/listeners.js
@@ -58,10 +58,12 @@ module.exports = {
         const subCommand = interaction.options.getSubcommand();
         
         if(subCommandGroup == "youtube") {
+            await interaction.deferReply({ ephemeral: true });
+
             if(subCommand == "create") {
                 let [rows, fields] = await promisePool.execute('SELECT * FROM Global.youtube_listeners WHERE created_by = ?', [interaction.user.id]);
                 if(rows.length >= 5) {
-                    return await interaction.reply({ content: "You can only have up to 5 youtube listeners.", ephemeral: true });
+                    return await interaction.editReply({ content: "You can only have up to 5 youtube listeners.", ephemeral: true });
                 }
 
                 const youtubeChannelID = interaction.options.getString('youtube');
@@ -73,17 +75,17 @@ module.exports = {
                     .catch((err => { console.log("Error fetching youtube feed: " + err); return; })); // Returns if there is an error
                     
                 if(!response || !response.data) { // Returns if there is no response or data
-                    return await interaction.reply({ content: "There was an error fetching the youtube channel. Please check the channel ID and try again.", ephemeral: true });
+                    return await interaction.editReply({ content: "There was an error fetching the youtube channel. Please check the channel ID and try again.", ephemeral: true });
                 };
 
                 if( response.data.pageInfo.totalResults <= 0 ) {
-                    return await interaction.reply({ content: "This channel ID does not exist or is invalid. Please check your spelling and try again.", ephemeral: true });
+                    return await interaction.editReply({ content: "This channel ID does not exist or is invalid. Please check your spelling and try again.", ephemeral: true });
                 }
 
                 // Check if the channelID already exists for this user
                 let [existingRows, existingFields] = await promisePool.execute('SELECT * FROM Global.youtube_listeners WHERE youtube_channel_id = ? AND discord_channel_id = ?', [youtubeChannelID, discordChannelID]);
                 if(existingRows.length > 0) {
-                    return await interaction.reply({ content: "You already have created this listener.", ephemeral: true });
+                    return await interaction.editReply({ content: "You already have created this listener.", ephemeral: true });
                 }
 
                 // Insert the new listener into the database
@@ -91,7 +93,7 @@ module.exports = {
                     .execute('INSERT INTO Global.youtube_listeners (youtube_channel_id, uploads_playlist_id, discord_channel_id, send_message, channel_img, created_by) VALUES (?, ?, ?, ?, ?, ?)', [youtubeChannelID, response.data.items[0].contentDetails.relatedPlaylists.uploads, discordChannelID, sendMessage, response.data.items[0].snippet.thumbnails.high.url, interaction.user.id])
                     .catch(err => { console.error("Error inserting youtube listener: ", err); });
                 
-                return await interaction.reply({ content: `You have successfully created a youtube listener for channel: https://www.youtube.com/channel/${youtubeChannelID} in <#${discordChannelID}>.`, ephemeral: true });
+                return await interaction.editReply({ content: `You have successfully created a youtube listener for channel: https://www.youtube.com/channel/${youtubeChannelID} in <#${discordChannelID}>.`, ephemeral: true });
             }
             else if(subCommand == "remove") {
                 const youtubeChannelID = interaction.options.getString('youtube');
@@ -100,21 +102,21 @@ module.exports = {
                 // Check if the listener exists for this user
                 let [existingRows, existingFields] = await promisePool.execute('SELECT * FROM Global.youtube_listeners WHERE youtube_channel_id = ? AND discord_channel_id = ? AND created_by = ?', [youtubeChannelID, discordChannelID, interaction.user.id]);
                 if(existingRows.length === 0) {
-                    return await interaction.reply({ content: "You do not have this listener created. Make sure everything is spelled correctly.", ephemeral: true });
+                    return await interaction.editReply({ content: "You do not have this listener created. Make sure everything is spelled correctly.", ephemeral: true });
                 }
                 // Remove the listener from the database
                 await promisePool
                     .execute('DELETE FROM Global.youtube_listeners WHERE youtube_channel_id = ? AND discord_channel_id = ? AND created_by = ?', [youtubeChannelID, discordChannelID, interaction.user.id])
                     .catch(err => { console.error("Error deleting youtube listener: ", err); });
 
-                return await interaction.reply({ content: `You have successfully removed the youtube listener for channel: https://www.youtube.com/channel/${youtubeChannelID} in <#${discordChannelID}>.`, ephemeral: true });
+                return await interaction.editReply({ content: `You have successfully removed the youtube listener for channel: https://www.youtube.com/channel/${youtubeChannelID} in <#${discordChannelID}>.`, ephemeral: true });
             }
             else if(subCommand == "list") {
                 // List all existing listeners for youtube channels you have created.
                 let [rows, fields] = await promisePool.execute('SELECT * FROM Global.youtube_listeners WHERE created_by = ?', [interaction.user.id]);
                 
                 if(rows.length === 0) {
-                    return await interaction.reply({ content: "You have no existing youtube listeners.", ephemeral: true });
+                    return await interaction.editReply({ content: "You have no existing youtube listeners.", ephemeral: true });
                 }
 
                 let response = "Your existing youtube listeners:\n";
@@ -122,11 +124,11 @@ module.exports = {
                     response += `- Channel: https://www.youtube.com/channel/${row.youtube_channel_id} | Discord Channel: <#${row.discord_channel_id}>\n`;
                 });
 
-                return await interaction.reply({ content: response, ephemeral: true });
+                return await interaction.editReply({ content: response, ephemeral: true });
             }
         }
         else {
-            return await interaction.reply({ content: "Invalid subcommand group.", ephemeral: true });
+            return await interaction.editReply({ content: "Invalid subcommand group.", ephemeral: true });
         }
     }
 }
